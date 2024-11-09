7c28ef6293e3e8582e3642e6d1aae63bc8183d1b
alev-ti <kainah999@yandex.ru>
2024-11-09 16:06:44 +0100

    feat: add sharing, centering and upscaling image

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -7,24 +7,75 @@
 
 import UIKit
 
-class SingleImageViewController: UIViewController {
+class SingleImageViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBAction private func didTapBackButton() {
-        dismiss(animated: true, completion: nil)
-    }
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
     
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
-    @IBOutlet private var imageView: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        scrollView.delegate = self
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        centerImageInScrollView()
+    }
+    
+    private func centerImageInScrollView() {
+        let visibleRectSize = scrollView.bounds.size
+        let contentSize = scrollView.contentSize
+        let xOffset = max(0, (visibleRectSize.width - contentSize.width) / 2)
+        let yOffset = max(0, (visibleRectSize.height - contentSize.height) / 2)
+        scrollView.contentInset = UIEdgeInsets(top: yOffset, left: xOffset, bottom: 0, right: 0)
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImageInScrollView()
+    }
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image else {
+            print("No image found")
+            return
+        }
+        let text = "Shared with love from ImageFeed App"
+        let vc = UIActivityViewController(
+            activityItems: [image, text],
+            applicationActivities: []
+        )
+        present(vc, animated: true, completion: nil)
+    }
 }
