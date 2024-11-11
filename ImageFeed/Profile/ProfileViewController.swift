08e5e66ed544ddb0c618ad408f3d86daa1dd8c4d
alev-ti <kainah999@yandex.ru>
2024-11-11 21:13:37 +0100

    feat: add profile view code layout

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,17 +1,90 @@
-//
-//  ProfileViewController.swift
-//  ImageFeed
-//
-//  Created by alevtine on 7.11.24..
-//
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet weak var avatar: UIImageView!
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var nick: UILabel!
-    @IBOutlet weak var descriptionText: UILabel!
     
-    @IBAction func onLogoutButton(_ sender: Any) {
+    private let profileImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let usernameLabel = UILabel()
+    private let statusLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        adjustView()
+        renderProfileImage()
+        renderProfileDetails()
+        renderLogoutButton()
     }
+    
+    private func adjustView() {
+        view.backgroundColor = UIColor(named: "YP Black")
+    }
+    
+    private func renderProfileImage() {
+        profileImageView.image = UIImage(named: "avatar")
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+    }
+    
+    private func renderProfileDetails() {
+        // Имя пользователя
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        nameLabel.textColor = UIColor(named: "YP White")
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        // Никнейм
+        usernameLabel.text = "@ekaterina_nov"
+        usernameLabel.font = UIFont.systemFont(ofSize: 13)
+        usernameLabel.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0) // #AEAFB4
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usernameLabel)
+        
+        // Статус
+        statusLabel.text = "Hello, world!"
+        statusLabel.font = UIFont.systemFont(ofSize: 13)
+        statusLabel.textColor = UIColor(named: "YP White")
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(statusLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            
+            usernameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            statusLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            statusLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8)
+        ])
+    }
+    
+    private func renderLogoutButton() {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "logout_btn")!,
+            target: self,
+            action: #selector(didTapButton)
+        )
+        button.tintColor = UIColor(named: "YP Red")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
+            button.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 40),
+            button.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    @objc
+    private func didTapButton() {}
 }
