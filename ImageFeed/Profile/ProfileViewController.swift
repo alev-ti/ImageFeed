ce41d869d2d0fb16f9342cf4127f4220f53932a0
alev-ti <kainah999@yandex.ru>
2024-11-13 17:08:52 +0100

    refactor: separate methods, set lazy vars

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,60 +2,80 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    private let profileImageView = UIImageView()
-    private let nameLabel = UILabel()
-    private let usernameLabel = UILabel()
-    private let statusLabel = UILabel()
+    private lazy var profileImageView: UIImageView = {
+        let image = UIImage(named: "avatar")
+        let view = UIImageView(image: image)
+        return view
+    }()
+
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.font = UIFont.boldSystemFont(ofSize: 23)
+        label.textColor = UIColor(named: "YP White")
+        return label
+    }()
+    
+    private lazy var usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_nov"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0) // #AEAFB4
+        return label
+    }()
+    
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor(named: "YP White")
+        return label
+    }()
+    
+    private lazy var logoutButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "logout_btn")!,
+            target: self,
+            action: #selector(didTapButton)
+        )
+        button.tintColor = UIColor(named: "YP Red")
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         adjustView()
-        renderProfileImage()
-        renderProfileDetails()
-        renderLogoutButton()
+        addSubviews()
+        setupConstraints()
     }
     
     private func adjustView() {
         view.backgroundColor = UIColor(named: "YP Black")
     }
     
-    private func renderProfileImage() {
-        profileImageView.image = UIImage(named: "avatar")
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(profileImageView)
+    private func addSubviews() {
+        let viewsArray: [UIView] = [
+            profileImageView,
+            nameLabel,
+            usernameLabel,
+            statusLabel,
+            logoutButton
+        ]
         
+        viewsArray.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
             profileImageView.widthAnchor.constraint(equalToConstant: 70),
-            profileImageView.heightAnchor.constraint(equalToConstant: 70)
-        ])
-    }
-    
-    private func renderProfileDetails() {
-        // Имя пользователя
-        nameLabel.text = "Екатерина Новикова"
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 23)
-        nameLabel.textColor = UIColor(named: "YP White")
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(nameLabel)
-        
-        // Никнейм
-        usernameLabel.text = "@ekaterina_nov"
-        usernameLabel.font = UIFont.systemFont(ofSize: 13)
-        usernameLabel.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0) // #AEAFB4
-        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(usernameLabel)
-        
-        // Статус
-        statusLabel.text = "Hello, world!"
-        statusLabel.font = UIFont.systemFont(ofSize: 13)
-        statusLabel.textColor = UIColor(named: "YP White")
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(statusLabel)
-        
-        NSLayoutConstraint.activate([
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            
             nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
             
@@ -63,25 +83,12 @@ final class ProfileViewController: UIViewController {
             usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
             
             statusLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            statusLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8)
-        ])
-    }
-    
-    private func renderLogoutButton() {
-        let button = UIButton.systemButton(
-            with: UIImage(named: "logout_btn")!,
-            target: self,
-            action: #selector(didTapButton)
-        )
-        button.tintColor = UIColor(named: "YP Red")
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
-        
-        NSLayoutConstraint.activate([
-            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
-            button.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
-            button.widthAnchor.constraint(equalToConstant: 40),
-            button.heightAnchor.constraint(equalToConstant: 40)
+            statusLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8),
+            
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            logoutButton.widthAnchor.constraint(equalToConstant: 40),
+            logoutButton.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
     
