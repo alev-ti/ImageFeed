5551913b80c0e066c73c9b9f3a29757af795c3d3
alev-ti <kainah999@yandex.ru>
2024-12-20 14:11:49 +0100

    fix: race handling replaced with current task checking

--- ImageFeed/OAuth2/OAuth2Service.swift
+++ ImageFeed/OAuth2/OAuth2Service.swift
@@ -8,24 +8,22 @@ final class OAuth2Service {
 
     private let tokenStorage = OAuth2TokenStorage()
     
-    private var activeTasks: [String: [OAuthTokenResult]] = [:]
     private var currentTask: URLSessionTask?
+    private var currentCode: String?
 
-    func fetchOAuthToken(
-        _ code: String, completion: @escaping OAuthTokenResult
-    ) {
+    func fetchOAuthToken(_ code: String, completion: @escaping OAuthTokenResult) {
         let completionOnTheMainThread: OAuthTokenResult = { result in
             DispatchQueue.main.async {
                 completion(result)
             }
         }
-        
-        if activeTasks[code] != nil {
-            activeTasks[code]?.append(completionOnTheMainThread)
+
+        if code == currentCode {
             return
-        } else {
-            activeTasks[code] = [completionOnTheMainThread]
         }
+
+        currentTask?.cancel()
+        currentCode = code
 
         var components = URLComponents(string: Constants.unsplashTokenURLString)
         components?.queryItems = [
@@ -39,41 +37,31 @@ final class OAuth2Service {
         guard let url = components?.url else {
             let error = URLError(.badURL)
             print("[OAuth2Service/fetchOAuthToken]: urlRequestError - failed to create URL")
-            completeAll(for: code, with: .failure(error))
+            completionOnTheMainThread(.failure(error))
+            currentCode = nil
             return
         }
 
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
 
-        currentTask?.cancel()
-
         let task = URLSession.shared.objectTask(for: request) { [weak self] (result: Result<OAuthTokenResponseBody, Error>) in
             defer {
-                self?.activeTasks.removeValue(forKey: code)
                 self?.currentTask = nil
+                self?.currentCode = nil
             }
 
             switch result {
             case .success(let responseBody):
                 self?.tokenStorage.token = responseBody.accessToken
-                self?.completeAll(for: code, with: .success(responseBody.accessToken))
+                completionOnTheMainThread(.success(responseBody.accessToken))
             case .failure(let error):
                 print("[OAuth2Service/fetchOAuthToken]: NetworkError - \(error.localizedDescription)")
-                self?.completeAll(for: code, with: .failure(error))
+                completionOnTheMainThread(.failure(error))
             }
         }
 
         currentTask = task
         task.resume()
-    }
-
-    private func completeAll(for code: String, with result: Result<String, Error>) {
-        if let completions = activeTasks[code] {
-            for completion in completions {
-                completion(result)
-            }
-        }
-        activeTasks.removeValue(forKey: code)
     }
 }
